70b60babd417b538bdbccacd4dce232d93863365
sugitakana <126451068+sugitakana@users.noreply.github.com>
2023-04-05 06:19:56 +0000

    [Add]商品詳細画面

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,12 +2,12 @@ class Item < ApplicationRecord
   has_one_attached :image
   has_many :genres
   
-  def get_image(width, height)
+  def get_image
    unless image.attached?
     file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
     image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
    end
-   image.variant(resize_to_limit: [width, height]).processed
+   image.variant(resize_to_limit: [250,250]).processed
   end
   
   validates :name, presence: true
