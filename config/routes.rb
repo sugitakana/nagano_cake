43e0bdfef3ba123684584e6835d100f26dc86484
sugitakana <126451068+sugitakana@users.noreply.github.com>
2023-04-06 12:34:36 +0000

    [Add]会員詳細画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-  end
+  
   devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
@@ -17,6 +13,7 @@ namespace :admin do
   root to: "homes#top"
   resources :items, only: [:show, :index, :new, :create, :edit, :update]
   resources :genres, only:[:index, :create, :edit, :update]
+  resources :customers, only:[:index, :edit, :update, :show]
 end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
