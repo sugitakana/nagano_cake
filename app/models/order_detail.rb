4be5c7ca0e37da8d3f98768a21d980e7e475ad08
sugitakana <126451068+sugitakana@users.noreply.github.com>
2023-04-08 15:21:44 +0000

    [Add]enum導入

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -2,6 +2,8 @@ class OrderDetail < ApplicationRecord
   
   belongs_to :order
   
+  enum production_status: { impossible_manufacture: 0,  waiting_manufacture: 1, manufacturing: 2, finish: 3 
+  
   validates :price, presence: true
   validates :quantity, presence: true
   validates :production_status, presence: true
