99053f8c94c254ddab2dad4b50da2a58782cbf56
sugitakana <126451068+sugitakana@users.noreply.github.com>
2023-04-09 05:14:13 +0000

    [Add]注文履歴一覧画面

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,8 +1,20 @@
 class Admin::OrdersController < ApplicationController
+  
+  def index
+    @orders = Order.all
+  end
+  
   def show
     @order = Order.find(params[:id])
     @order_details = @order.order_details
     @total = 0
+  end
+  
+  def update
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
+    order.update(order_params)
+    redirect_to admin_order_path(@order)
   end
   
   private
