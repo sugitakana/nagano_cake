1e7b5a3f79c8e82ad742125e1d9f9f3122e47c51
sugitakana <126451068+sugitakana@users.noreply.github.com>
2023-04-14 13:02:20 +0000

    [Add]cart_items_controller

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,12 +1,22 @@
 class Public::CartItemsController < ApplicationController
   
   def index
-    @cart_items = current_member.cart_items.all
+    @cart_items = CartItems.all
   end
   
   def create
-    @cart_item = current_member.cart_items.new(cart_item_params)
-    
+    @cart_item = CartItems.new(cart_item_params)
+    if CartItems.find_by(item_id:params[:cart_item][:item_id]).present?
+      cart_item = CartItems.find_by(item_id: params[:cart_item][:item_id])
+      cart_item.amount += params[:cart_item][:amount].to_i
+      cart_item.save
+      redirect_to cart_items_path
+    elsif @cart_item.save
+      @cart_items = CartItems.all
+      redirect_to cart_items_path
+    else
+      render "customers/items/show"
+    end
   end
   
   private
