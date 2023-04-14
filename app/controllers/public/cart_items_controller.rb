1ede37ecd8ab1c4cb9b68cd52727c3a84e7844bc
sugitakana <126451068+sugitakana@users.noreply.github.com>
2023-04-14 13:26:15 +0000

    [Add]カートの更新、カート内の商品の削除機能

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -5,8 +5,8 @@ class Public::CartItemsController < ApplicationController
   end
   
   def create
-    @cart_item = CartItems.new(cart_item_params)
-    if CartItems.find_by(item_id:params[:cart_item][:item_id]).present?
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+    if CartItems.find_by(item_id: params[:cart_item][:item_id]).present?
       cart_item = CartItems.find_by(item_id: params[:cart_item][:item_id])
       cart_item.amount += params[:cart_item][:amount].to_i
       cart_item.save
@@ -17,6 +17,24 @@ class Public::CartItemsController < ApplicationController
     else
       render "customers/items/show"
     end
+  end
+  
+  def update
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to cart_items_path
+  end
+  
+  def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to cart_items_path
+  end
+  
+  def destroy_all
+    @cart_item = current_customer.cart_items
+    @cart_item.destroy_all
+    redirect_to cart_items_path
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -14,20 +14,24 @@ Rails.application.routes.draw do
     resources :customers, only:[:edit, :update]
     resources :addresses, only:[:index, :create, :edit, :update, :destroy]
     resources :items, only:[:index, :show]
-    resources :cart_items, only:[:index, :create, :update, :destroy]
+    resources :cart_items, only:[:index, :create, :update, :destroy] do
+       collection do
+        delete "destroy_all"
+      end
+    end
   end
 
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
 
-namespace :admin do
-  root to: 'homes#top'
-  resources :items, only: [:show, :index, :new, :create, :edit, :update]
-  resources :genres, only:[:index, :create, :edit, :update]
-  resources :customers, only:[:index, :edit, :update, :show]
-  resources :orders, only:[:show, :update]
-  resources :order_details, only:[:update]
-end
+ namespace :admin do
+    root to: 'homes#top'
+    resources :items, only: [:show, :index, :new, :create, :edit, :update]
+    resources :genres, only:[:index, :create, :edit, :update]
+    resources :customers, only:[:index, :edit, :update, :show]
+    resources :orders, only:[:show, :update]
+    resources :order_details, only:[:update]
+ end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
