576785b1f53be42bfcd6f0eda40f9bf79949505b
sugitakana <126451068+sugitakana@users.noreply.github.com>
2023-04-15 11:35:59 +0000

    [Add]カート画面

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,13 +1,14 @@
 class Public::CartItemsController < ApplicationController
-  
+
   def index
-    @cart_items = CartItems.all
+    @cart_items = current_cosutmer.cart_items.all
+    @total_price = 0
   end
-  
+
   def create
     @cart_item = current_customer.cart_items.new(cart_item_params)
-    if CartItems.find_by(item_id: params[:cart_item][:item_id]).present?
-      cart_item = CartItems.find_by(item_id: params[:cart_item][:item_id])
+    if current_cosutmer.cart_item.find_by(item_id: params[:cart_item][:item_id]).present?
+      cart_item = current_cosutmer.cart_item.find_by(item_id: params[:cart_item][:item_id])
       cart_item.amount += params[:cart_item][:amount].to_i
       cart_item.save
       redirect_to cart_items_path
@@ -18,27 +19,27 @@ class Public::CartItemsController < ApplicationController
       render "customers/items/show"
     end
   end
-  
+
   def update
     @cart_item = CartItem.find(params[:id])
     @cart_item.update(cart_item_params)
     redirect_to cart_items_path
   end
-  
+
   def destroy
     @cart_item = CartItem.find(params[:id])
     @cart_item.destroy
     redirect_to cart_items_path
   end
-  
+
   def destroy_all
     @cart_item = current_customer.cart_items
     @cart_item.destroy_all
     redirect_to cart_items_path
   end
-  
+
   private
     def cart_item_params
-        params.require(:cart_item).permit(:item_id, :price, :amount)
+        params.require(:cart_item).permit(:item_id, :amount)
     end
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -3,5 +3,9 @@ class CartItem < ApplicationRecord
   belongs_to :item
   belongs_to :customer
   
+  def subtotal
+    item.with_tax_price * amount
+ end
+  
   validates :amount, presence: true
 end
