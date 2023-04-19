439a86cf7e8b1592d2f45ba14fb82ec7ef61d056
sugitakana <126451068+sugitakana@users.noreply.github.com>
2023-04-19 12:35:40 +0000

    [Add]注文履歴詳細画面

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,14 +1,14 @@
 class OrderDetail < ApplicationRecord
-  
+
   belongs_to :order
   belongs_to :item
-  
+
   enum production_status: { impossible_manufacture: 0,  waiting_manufacture: 1, manufacturing: 2, finish: 3 }
-  
+
   def subtotal
     item.with_tax_price * quantity
   end
-  
+
   validates :price, presence: true
   validates :quantity, presence: true
   validates :production_status, presence: true
