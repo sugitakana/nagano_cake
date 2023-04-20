4a936b5c704a0de81a1988d7faaf42369144b16c
sugitakana <126451068+sugitakana@users.noreply.github.com>
2023-04-20 09:09:24 +0000

    [Add]ヘッダーのレイアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,19 +4,19 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     case resource
       when Admin
-        admin_root_path
+               admin_root_path
       when Customer
-        root_path
+               root_path
     end
   end
   
   def after_sign_out_path_for(resource)
     case resource
-      when Admin
-        new_admin_session_path
-      when Customer
-        root_path
-    end
+      when :admin
+               new_admin_session_path
+      when :customer
+               root_path
+    end 
   end
 
   protected
