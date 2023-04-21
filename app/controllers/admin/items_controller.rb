43c78578618f49079815f3a897ba5beb2c022992
sugitakana <126451068+sugitakana@users.noreply.github.com>
2023-04-21 10:54:29 +0000

    [Fix]管理者の商品関連画面

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,41 +1,44 @@
 class Admin::ItemsController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def index
     @items = Item.page(params[:page]).per(10)
   end
-  
+
   def new
     @item= Item.new
   end
-  
+
   def create
     @item = Item.new(item_params)
-    @item.save
-    redirect_to admins_item_path(@item)
+    if @item.save
+      flash[:notice] = "商品情報を登録しました"
+      redirect_to admin_item_path(@item)
+    else
+      render 'new'
+    end
   end
-  
+
   def show
     @item = Item.find(params[:id])
   end
-  
+
   def edit
     @item = Item.find(params[:id])
   end
-  
+
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
        flash[:notice] = "商品情報を更新しました"
-       redirect_to admins_item_path(@item)
+       redirect_to admin_item_path(@item)
     else
       render 'edit'
     end
   end
-  
+
    private
     def item_params
       params.require(:item).permit(:genre_id, :name, :introduction, :price, :image, :is_active)
     end
 end
- 
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -8,7 +8,7 @@ class Public::ItemsController < ApplicationController
   def show
     @genres = Genre.all
     @item = Item.find(params[:id])
-    @cart_item = Cartitem.new
+    @cart_item = CartItem.new
   end
   
   private
