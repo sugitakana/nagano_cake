bd71a5a263e4a9d5239d599570e89615217fd886
sugitakana <126451068+sugitakana@users.noreply.github.com>
2023-04-28 13:29:10 +0000

    [Fix]レイアウト修正

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,10 +1,10 @@
 class Public::HomesController < ApplicationController
-  
+
   def top
     @genres = Genre.all
-    @items = Item.all.order(created_at: :desc)
+    @items = Item.page(params[:page]).per(4).order(created_at: :desc)
   end
-  
+
   def about
   end
 end
